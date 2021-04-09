ff19ec8e84fab73bcc3187b0326ceeb076b76f0e
李佳龙 <5145565+lijialongs@user.noreply.gitee.com>
2021-04-09 21:22:26 +0800

    删除文件 carver-tp

--- carver-tp/app/admin/lang/zh-cn/link.lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$info['link_apply_fail'] = '申请失败，请稍后重试!';
-$info['link_apply_confirm'] = '申请已发出,等待博主确认!';
-$info['link_update_success'] = '申请状态已修改!';
-$info['link_update_fail'] = '申请状态修改失败，请稍后重试!';
-return $info;
