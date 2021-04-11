f0d5bec9eaa4608248d61df6302ccb796f47d4b2
lijialongs <lijialong8012@163.com>
2021-04-11 13:57:32 +0800

    邮件设置修复

--- app/admin/config/email.php
+++ app/admin/config/email.php
@@ -2,11 +2,9 @@
 /**
  * Created by PhpStorm.
  * User: Carver
- * Date: 2020/9/27
- * Time: 21:51
  */
 return [
 	'HOST'=>'smtp.qq.com',
-	'USERNAME'=>'lijialong8012@qq.com',
-	'PASSWORD'=>'gxvxkjfdhtbhchjh',
+	'USERNAME'=>'',
+	'PASSWORD'=>'',
 ];
